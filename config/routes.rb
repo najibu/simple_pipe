292169e3404a079d1856bb1fd95f1fceb34a977c
najibu <ns.najibu@gmail.com>
2016-11-23 15:46:23 +0000

    Installed apartment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
+class SubdomainPresent
+  def self.matches?(request)
+    request.subdomain.present?
+  end
+end
+
+class SubdomainBlank
+  def self.matches?(request)
+    request.subdomain.blank?
+  end
+end
+
 Rails.application.routes.draw do
+  resources :contacts
+  constraints(SubdomainPresent) do
+    root 'contacts#index', as: :subdomain_root
+    devise_for :users
+  end
 
-  devise_for :users
-  root 'welcome#index'
-
-  resources :accounts
-  
+  constraints(SubdomainBlank) do
+    root "welcome#index"
+    resources :accounts, only: [:new, :create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123141411) do
+ActiveRecord::Schema.define(version: 20161123151856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20161123141411) do
   create_table "accounts", force: :cascade do |t|
     t.string   "subdomain"
     t.integer  "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "company"
+    t.text     "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
